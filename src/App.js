4bc1385a48f811a5b20f18efa95404c0dd77ed42
johnxadams <johnxadams93@gmail.com>
2022-08-14 13:56:03 +0200

    date added , data inject

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 // url
-import { url } from "./api-url";
+import { apiKey } from "./api-key";
 
+//librabries
 import axios from "axios";
+
 // styles
 import "./App.css";
 import "./scss/_layout.scss";
@@ -10,14 +12,28 @@ import "./scss/_layout.scss";
 function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
 
-  const searchLocation = (e) => {};
+  const searchLocation = (e) => {
+    if (e.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+    }
+  };
+  const handleChange = (e) => {
+    setLocation(e.target.value);
+    console.log(e.target.value);
+  };
+  const current = new Date();
+  const date = `${current.getDate()} ${current.getMonth() + 1}`;
   return (
     <div className="app">
       <div className="search">
         <input
           value={location}
-          onChange={(e) => setLocation(e.target.value)}
+          onChange={handleChange}
           onKeyPress={searchLocation}
           placeholder="Enter Location"
           type="text"
@@ -26,7 +42,7 @@ function App() {
       <section className="weather-info-container">
         <div className="top">
           <div className="location">
-            <p>Leipzig</p>
+            <p>{data.name}</p>
           </div>
           <div className="temperature">
             <h3>50 °C</h3>
@@ -35,21 +51,29 @@ function App() {
             <p>Sunny Clear</p>
           </div>
         </div>
-        <div className="main"></div>
-        <div className="bottom">
-          <div className="feels">
-            <p>Feels Like</p>
-            <p>60 °C</p>
-          </div>
-          <div className="humidity">
-            <p>40%</p>
-            <p>Humidity</p>
-          </div>
-          <div className="wind">
-            <p>20 km/h</p>
-            <p>Wind Speed</p>
-          </div>
-        </div>
+        {data.name && (
+          <>
+            <div className="main">
+              <p> Today {date}</p>
+            </div>
+            <div className="bottom">
+              <div className="feels">
+                <p>Feels Like</p>
+                <p>60 °C</p>
+              </div>
+              <div className="humidity">
+                {data.main ? (
+                  <p className="bold">{data.main.humidity}%</p>
+                ) : null}
+                <p>Humidity</p>
+              </div>
+              <div className="wind">
+                <p>20 km/h</p>
+                <p>Wind Speed</p>
+              </div>
+            </div>
+          </>
+        )}
       </section>
     </div>
   );
