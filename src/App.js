126c77cabb716196f7f58adea07eb146d7eecb6b
johnxadams <johnxadams93@gmail.com>
2022-08-14 11:10:32 +0200

    api url added, API-key injected; axios install and imported

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">ONe
-    </div>
-  );
+  const url = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=e238de25e3101c15f4fae18685f436a5`;
+
+  return <div className="App">ONe</div>;
 }
 
 export default App;
