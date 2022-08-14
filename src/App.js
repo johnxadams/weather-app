3253a0ea3d16e9543f721e9e5a7abcaef8b8b804
johnxadams <johnxadams93@gmail.com>
2022-08-14 11:47:20 +0200

    rfc mhtl tag

--- src/App.js
+++ src/App.js
@@ -10,10 +10,47 @@ function App() {
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=e238de25e3101c15f4fae18685f436a5`;
 
+  const searchLocation = (e) => {};
   return (
-    <div className="App">
-      ONe
-      <h3>Blue</h3>
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+          onKeyPress={searchLocation}
+          placeholder="Enter Location"
+          type="text"
+        />
+      </div>
+      <section className="weather-info-container">
+        <div className="top">
+          <div className="location">
+            <p>Leipzig</p>
+          </div>
+          <div className="temperature">
+            <p>50 °C</p>
+          </div>
+          <div className="description">
+            <p>Sunny Clear</p>
+          </div>
+        </div>
+        <div className="main"></div>
+        <div className="bottom">
+          <div className="feels">
+            <p>60 °C</p>
+          </div>
+          <div className="bottom">
+            <div className="humidity">
+              <p>40%</p>
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              <p>20 km/h</p>
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
