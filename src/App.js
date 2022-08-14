c561587f5d83f6f7297ba6790e8d9237f8ba2ffb
johnxadams <johnxadams93@gmail.com>
2022-08-14 11:24:10 +0200

    input init, searchLocation int

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./App.css";
 
+import "./scss/_layout.scss";
+
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
   const url = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=e238de25e3101c15f4fae18685f436a5`;
 
-  return <div className="App">ONe</div>;
+  return (
+    <div className="App">
+      ONe
+      <h3>Blue</h3>
+    </div>
+  );
 }
 
 export default App;
